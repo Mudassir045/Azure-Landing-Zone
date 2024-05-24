79ecf08382eb93d367a6bd5f2aa571dc945cc781
Mudassir Ahmad <87496849+Mudassir045@users.noreply.github.com>
2024-05-24 18:14:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,97 +1,96 @@
 variable "subscription_id" {
   type      = string
-  sensitive = true
-  default   = ""
+#  sensitive = true
 }
 
 variable "tenant_id" {
   type      = string
-  sensitive = true
-  default   = ""
+#  sensitive = true
+#  default   = ""
 }
 
 variable "client_id" {
   type      = string
-  sensitive = true
-  default   = ""
+#  sensitive = true
+#  default   = ""
 }
 
 variable "client_secret" {
   type      = string
-  sensitive = true
-  default   = ""
+#  sensitive = true
+#  default   = ""
 }
 
 variable "management_group_display_name" {
   type    = string
-  default = "DEV-01"
+#  default = "DEV-01"
 }
 
 variable "policy_display_names" {
   description = "A list of display names for the built-in policies."
   type        = list(string)
-  default = ["Audit VMs that do not use managed disks",
-    "Network interfaces should not have public IPs",
-    "Subnets should be associated with a Network Security Group",
-    "Network Watcher should be enabled",
-    "Management ports should be closed on your virtual machines",
-    "Auto provisioning of the Log Analytics agent should be enabled on your subscription",
-    #"Enable Azure Monitor for VMs with Azure Monitoring Agent(AMA)",
-    "Audit usage of custom RBAC roles",
-    "A maximum of 3 owners should be designated for your subscription",
-    "Enable Microsoft Defender for Cloud on your subscription",
-    #"Configure Log Analytics workspace and automation account to centralize logs and monitoring"
-  ]
+#  default = ["Audit VMs that do not use managed disks",
+#    "Network interfaces should not have public IPs",
+#    "Subnets should be associated with a Network Security Group",
+#    "Network Watcher should be enabled",
+#    "Management ports should be closed on your virtual machines",
+#    "Auto provisioning of the Log Analytics agent should be enabled on your subscription",
+#    #"Enable Azure Monitor for VMs with Azure Monitoring Agent(AMA)",
+#    "Audit usage of custom RBAC roles",
+#    "A maximum of 3 owners should be designated for your subscription",
+#    "Enable Microsoft Defender for Cloud on your subscription",
+#    #"Configure Log Analytics workspace and automation account to centralize logs and monitoring"
+#  ]
 }
 
 variable "budget_resource_group_name" {
   type    = string
-  default = "budget-rg"
+#  default = "budget-rg"
 }
 
 variable "budget_action_group_name1" {
   type    = string
-  default = "Budgetag1"
+#  default = "Budgetag1"
 }
 
 variable "budget_action_group_name1_short_name" {
   type    = string
-  default = "Budgetag1"
+#  default = "Budgetag1"
 }
 
 variable "Subscription_budget_alert_name" {
   type    = string
-  default = "Subscription-budget-alert"
+#  default = "Subscription-budget-alert"
 }
 
 variable "budget_alert_amount" {
   type    = number
-  default = 5000
+#  default = 5000
 }
 
 variable "budget_alert_time_grain" {
   type    = string
-  default = "Monthly"
+#  default = "Monthly"
 }
 
 variable "time_period_start_date" {
   type    = string
-  default = "2024-05-01T00:00:00Z"
+#  default = "2024-05-01T00:00:00Z"
 }
 
 variable "time_period_end_date" {
   type    = string
-  default = "2029-05-01T00:00:00Z"
+#  default = "2029-05-01T00:00:00Z"
 }
 
 variable "budget_notification_enabled" {
   type    = bool
-  default = true
+#  default = true
 }
 
 variable "budget_notification_operator" {
   type    = string
-  default = "EqualTo"
+#  default = "EqualTo"
 }
 
 # variable "budget_notification_threshold" {
@@ -103,39 +102,39 @@ variable "budget_notification_operator" {
 variable "contact_emails" {
   description = "A list of display names of emails."
   type        = list(string)
-  default = [
-    "abc@gmail.com",
-    "xyz@gmail.com",
-  ]
+#  default = [
+#    "abc@gmail.com",
+#   "xyz@gmail.com",
+#  ]
 }
 
 variable "tags" {
   description = "A map of tags to assign to the resource"
   type        = map(string)
-  default = {
-    Environment  = "Development"
-    CreatedBy    = "Mudassir"
-    ApprovedBy   = "Mudassir"
-    CreationDate = "23 May 2024"
-  }
+#  default = {
+#    Environment  = "Development"
+#    CreatedBy    = "Mudassir"
+#    ApprovedBy   = "Mudassir"
+#    CreationDate = "23 May 2024"
+#  }
 }
 
 variable "resource_group_name" {
   type        = string
   description = "This defines the resource group name"
-  default     = "DEV-RG-01"
+#  default     = "DEV-RG-01"
 }
 
 variable "location" {
   type        = string
   description = "This defines the location"
-  default     = "eastus"
+#  default     = "eastus"
 }
 
 variable "virtual_network_name" {
   type        = string
   description = "This defines the name of the virtual network"
-  default     = "DEV-VNET-01"
+#  default     = "DEV-VNET-01"
 }
 
 # variable "virtual_network_address_space" {
@@ -146,16 +145,16 @@ variable "virtual_network_name" {
 
 variable "admin_username" {
   type    = string
-  default = ""
+#  default = ""
 }
 
 variable "admin_password" {
   type      = string
-  sensitive = true
-  default   = ""
+#  sensitive = true
+#  default   = ""
 }
 
 variable "container_name" {
   type    = string
-  default = "dev"
+#  default = "dev"
 }
