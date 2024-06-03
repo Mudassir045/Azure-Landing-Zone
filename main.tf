259240e6e850ce7317f3aba1cfe7d3ef65e6535a
Mudassir Ahmad <87496849+Mudassir045@users.noreply.github.com>
2024-06-03 11:13:46 +0530

    Update tags on main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "azurerm_resource_group" "DEV-RG-01" {
 resource "azurerm_virtual_network" "DEV-VNET-01" {
   name                = var.virtual_network_name
   location            = var.location
+  tags                = var.tags
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16", ]
   depends_on = [
@@ -19,6 +20,7 @@ resource "azurerm_subnet" "DEV-Subnet-01" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = ["10.0.0.0/24"]
+  tags                 = var.tags
   depends_on = [
     azurerm_virtual_network.DEV-VNET-01
   ]
@@ -28,6 +30,7 @@ resource "azurerm_network_security_group" "DEV-NSG-01" {
   name                = "DEV-NSG-01"
   location            = var.location
   resource_group_name = var.resource_group_name
+  tags                = var.tags
 
   security_rule {
     name                       = "AllowRDP"
